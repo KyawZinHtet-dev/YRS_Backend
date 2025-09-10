27b27f05dacd7b5f55fb595335cf87aadd913f0f
kyawZin <nobodymr246@gmail.com>
2025-09-10 19:48:54 +0630

    🚧 [WIP] Finished User Portal Profile API

--- app/Http/Controllers/Api/UserPortal/ProfileController.php
+++ app/Http/Controllers/Api/UserPortal/ProfileController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\UserPortal;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserPortal\ProfileResource;
+use App\Services\ResponseService;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function profile()
     {
-        return auth()->user();
+        $user = auth()->guard('user_api')->user();
+        return ResponseService::success(data: new ProfileResource($user));
     }
 }
